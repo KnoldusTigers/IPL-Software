a29f9e2d8f58a000ed9a2edea514a61652b5b999
Akshit <akshit.kumar@knoldus.com>
2021-12-22 12:56:34 +0530

    add validations and fix all bugs

---
+++ src/main/java/com/dao/MatchRepo.java
@@ -0,0 +1,34 @@
+package com.dao;
+
+import com.model.MatchModel;
+import com.model.TeamModel;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The interface Match repo.
+ */
+public interface MatchRepo extends CrudRepository<MatchModel, Integer> {
+    /**
+     * Find by venue optional.
+     *
+     * @param venue the venue
+     * @return the optional
+     */
+//    List<PlayersModel> findByTeamId(String team_id);
+    Optional<MatchModel> findByVenue(String venue);
+
+    /**
+     * Find by scheduledate optional.
+     *
+     * @param date the date
+     * @return the optional
+     */
+    Optional<MatchModel> findByScheduledate(String date);
+
+    public Optional<MatchModel> findByTeam1(TeamModel team1);
+    List<MatchModel> findAllByTeam1(TeamModel team1);
+
+}
